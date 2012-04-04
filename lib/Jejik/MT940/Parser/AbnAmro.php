bdd465611bc86720b7a161ee99c37951e15d2c01
Nanne Huiges <nanne@huiges.nl>
2012-04-04 13:58:14 +0200

    Improved contraAccount and accountholder SEPA is now recognized too (bic/iban + name), and better accountholder support is added

--- lib/Jejik/MT940/Parser/AbnAmro.php
+++ lib/Jejik/MT940/Parser/AbnAmro.php
@@ -61,9 +61,10 @@ class AbnAmro extends AbstractParser
         }
     }
 
+
     /**
      * Get the contra account from a transaction
-     *
+     * For SEPA it returns BIC/IBAN numbers
      * @param array $lines The transaction text at offset 0 and the description at offset 1
      * @return string|null
      */
@@ -72,21 +73,23 @@ class AbnAmro extends AbstractParser
         if (!isset($lines[1])) {
             return null;
         }
-
+        if(preg_match('/SEPA OVERBOEKING\s*IBAN: ([0-9a-zA-Z]*).*\n?.*BIC: ([0-9a-zA-Z]*).*\n?.*NAAM: ([^\x0D]*)/',$lines[1],$match)){
+        	return trim($match[2]).'/'.trim($match[1]);
+        }
         if (preg_match('/^([0-9.]{11,14}) /', $lines[1], $match)) {
             return str_replace('.', '', $match[1]);
         }
 
-        if (preg_match('/^GIRO([0-9 ]{9}) /', $lines[1], $match)) {
-            return trim($match[1]);
+        if (preg_match('/^GIRO\s*(\d+)\s/', $lines[1], $match)) {
+            return $match[1];
         }
 
         return null;
     }
     
     /**
-    * Get the contra account from a transaction
-    *
+    * Get the contra account Holder from a transaction
+    * For joined accounts suffix "CJ" or suffix/infix "EO" is kept to indicate shared accounts
     * @param array $lines The transaction text at offset 0 and the description at offset 1
     * @return string|null
     */
@@ -96,16 +99,28 @@ class AbnAmro extends AbstractParser
     		return null;
     	}
     	
-    	if (preg_match('/^[0-9.]{11,14}\s+(.+?)\s\s+/', $lines[1], $match)) {
-        	return trim($match[1]);
-        }
-        if (preg_match('/^GIRO[0-9 ]{9}\s+(.+?)\s\s+/', $lines[1], $match)) {
-        	return trim($match[1]);
-        }
-    
+    	if(preg_match('/SEPA OVERBOEKING\s*IBAN: ([0-9a-zA-Z]*).*\n?.*BIC: ([0-9a-zA-Z]*).*\n?.*NAAM: ([^\x0D]*)/',$lines[1],$match)){
+    		return trim($match[3]);    		
+    	}else{    	
+	    	//name is on either line 1 or line2.
+	    	if(substr($lines[1],0,4) == "GIRO"){
+	    		$line1 = substr($lines[1],5,27);
+	    		$line2 = substr($lines[1],31,31);
+	    	}else{
+	    		$line1 = substr($lines[1],0,31);
+	    		$line2 = substr($lines[1],31,31);
+	    	}
+	    	
+	    	if(preg_match('/^\s*[0-9.]+\s+(\S.*)/',$line1,$match)){//suddenly, a none-whitespace appears!
+	    		return trim($match[1]);
+	    	}elseif(preg_match('/^\s*[0-9.]+\s+$/',$line1)){//line1 had only accountnr. Name is on line2
+	    		if(preg_match('/^\s*([^\x0D]*)/',$line2,$match)){
+	    			return trim($match[1]);
+	    		}
+	    	}
+    	}
     	return null;
     }
-    
     
     /**
     * Create a Transaction from MT940 transaction text lines
